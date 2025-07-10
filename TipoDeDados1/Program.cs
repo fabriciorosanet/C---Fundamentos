6aef15235d806c7d682c8e0b60eb7a8f22b02ca5
rosa <fabricio.rosa@ssys.com.br>
2025-07-10 12:56:32 -0500

    feat: Revendo os tipos de dados

---
+++ TipoDeDados1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Declarando Variaveis Numericas");
+Console.WriteLine();
+// Declarando variaveis
+byte valor1 = 255;
+sbyte valor2 = -128;
+int valor3 = -211111155;
+uint valor4 = 211111155;
+long valor5 = -9223372036854775808;
+
+const int valorConstante = 1000;
+
+int valor6 = 1000, valor7 = 1000, valor8 = 1000, valor9 = 1000;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+Console.WriteLine(valor4);
+Console.WriteLine(valor5);
+Console.WriteLine(valor6);
+Console.WriteLine(valor7);
+Console.WriteLine(valor8);
+Console.WriteLine(valor9);
+Console.WriteLine(valorConstante);
+
+Console.ReadLine();
---
+++ TipoDeDados2/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Atributos valores a double, float e decimal");
+Console.WriteLine();
+double valor1 = 1.234;
+float valor2 = 1.234f;
+decimal valor3 = 1.234m;
+
+Console.WriteLine(valor1);
+Console.WriteLine(valor2);
+Console.WriteLine(valor3);
+
+Console.ReadLine();
+
+Console.WriteLine("Comparando a precisão dos tipos de dados floar, double e decimal");
+
+float valorFloat = 1f / 3f;
+double valorDouble = 1d / 3d;
+decimal valorDecimal = 1m / 3m;
+
+Console.WriteLine(valorFloat);
+Console.WriteLine(valorDouble);
+Console.WriteLine(valorDecimal);
+
+Console.ReadLine();
