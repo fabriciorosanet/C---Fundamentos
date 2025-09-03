196443b22c40d84ede193ed828f06178d53b87af
Fabrício Rosa <161895481+fabriciorosanet@users.noreply.github.com>
2025-09-02 22:59:03 -0300

    Feat: OperadoresLogicos

---
+++ OperadoresLogicos1/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, World!");
+
+bool c1 = 10 >= 15; //false
+bool c2 = 20 != 15; //true
+bool resultado;
+
+Console.WriteLine($"c1 = {c1}");
+Console.WriteLine($"c2 = {c2}");
+
+//Operador (AND) = &&
+
+resultado = c1 && c2;
+Console.WriteLine("Operador AND(&&) : " + resultado);
+
+//Operador (OR) = ||
+
+resultado = c1 || c2;
+Console.WriteLine("Operador OR(||) : " + resultado);
+
+//Operador (NOT) = !
+resultado = !c1;
+Console.WriteLine("Operador NOT(!) : " + resultado);
