d1b69ce09615ae672bda75c99613150671191937
Fabrício Rosa <161895481+fabriciorosanet@users.noreply.github.com>
2025-09-04 21:39:48 -0300

    Feat: Estrutura IF e Else

---
+++ Estrutura_IF_ElSE/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("## Estrutura de Controle IF e Else");
+
+int aluno = 0;
+Console.Write("Digite a nota do aluno: ");
+aluno = Convert.ToInt32(Console.ReadLine());
+if (aluno > 5) 
+{
+    Console.WriteLine("Aluno Aprovado");
+}
+else
+{
+    Console.WriteLine("Aluno Reprovado");
+}
+Console.WriteLine("Fim do Programa");
